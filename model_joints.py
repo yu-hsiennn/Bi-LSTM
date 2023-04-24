e8b4cdffdf40784e5da2570470fba3ad51b99aab
yu-hsiennn <fsps25896@gmail.com>
2023-04-24 17:50:16 +0800

    add V6 joints definition

--- model_joints.py
+++ model_joints.py
@@ -9,6 +9,200 @@ Definition:
 - left leg: Neck, Pelvis, RightThigh, LeftThigh, LeftKnee, LeftAnkle (6 joints)
 - right leg: Neck, Pelvis, RightThigh, LeftThigh, RightKnee, RightAnkle (6 joints)
 
+- left hand: "LeftArm", "LeftHand",
+            "LeftHandThumb1", 
+            "LeftHandIndex1",
+            "LeftHandMiddle1",
+            "LeftHandRing1",
+            "LeftHandPinky1" (7 joints)
+
+- left thumb: "LeftHandThumb1", "LeftHandThumb2", "LeftHandThumb3" (3 joints)
+- left index: "LeftHandIndex1", "LeftHandIndex2", "LeftHandIndex3" (3 joints)
+- left middle: "LeftHandMiddle1", "LeftHandMiddle2", "LeftHandMiddle3" (3 joints)
+- left ring: "LeftHandRing1", "LeftHandRing2", "LeftHandRing3" (3 joints)
+- left pinky: "LeftHandPinky1", "LeftHandPinky2", "LeftHandPinky3" (3 joints)
+
+- right hand: "RightArm", "RightHand",
+            "RightHandThumb1", 
+            "RightHandIndex1", 
+            "RightHandMiddle1", 
+            "RightHandRing1", 
+            "RightHandPinky1" (7 joints)
+
+- right thumb: "RightHandThumb1", "RightHandThumb2", "RightHandThumb3" (3 joints)
+- right index: "RightHandIndex1", "RightHandIndex2", "RightHandIndex3" (3 joints)
+- right middle: "RightHandMiddle1", "RightHandMiddle2", "RightHandMiddle3" (3 joints)
+- right ring: "RightHandRing1", "RightHandRing2", "RightHandRing3" (3 joints)
+- right pinky: "RightHandPinky1", "RightHandPinky2", "RightHandPinky3" (3 joints)
+
+- left foot: LeftKnee, LeftAnkle, LeftToeBase (3 joints)
+- right foot: RightKnee, RightAnkle, RightToeBase (3 joints)
+"""
+class JointDefV6:
+    def __init__(self) -> None:
+        self.n_joints_part = {}
+        self.n_joints_part['Full_Body'] = 141
+
+        self.n_joints_part['Torso'] = 21
+        self.n_joints_part['LeftArm'] = 18
+        self.n_joints_part['RightArm'] = 18
+        self.n_joints_part['LeftLeg'] = 18
+        self.n_joints_part['RightLeg'] = 18
+        
+        self.n_joints_part['LeftHand'] = 21
+        self.n_joints_part['RightHand'] = 21
+
+        self.n_joints_part['LeftThumb'] = 9
+        self.n_joints_part['LeftIndex'] = 9
+        self.n_joints_part['LeftMiddle'] = 9
+        self.n_joints_part['LeftRing'] = 9
+        self.n_joints_part['LeftPinky'] = 9
+        self.n_joints_part['RightThumb'] = 9
+        self.n_joints_part['RightIndex'] = 9
+        self.n_joints_part['RightMiddle'] = 9
+        self.n_joints_part['RightRing'] = 9
+        self.n_joints_part['RightPinky'] = 9
+
+        self.n_joints_part['LeftFoot'] = 9
+        self.n_joints_part['RightFoot'] = 9
+
+        self.part_list = ['LeftArm', 'RightArm', 'LeftLeg', 'RightLeg', 'Torso',
+                        'LeftHand', 'RightHand',
+                        'LeftThumb', 'LeftIndex', 'LeftMiddle', 'LeftRing', 'LeftPinky',
+                        'RightThumb', 'RightIndex', 'RightMiddle', 'RightRing', 'RightPinky',
+                        'LeftFoot', 'RightFoot']
+
+    # concatenate the data it according human joints definition 
+    def cat_torch(self, part, data):
+        if part == 'Torso':
+            part_data = torch.cat((data[:, 0:9], data[:, 15:18], data[:, 24:30], data[:, 36:39]), axis=1)
+        elif part == 'LeftArm':
+            part_data = torch.cat((data[:, 3:9], data[:, 15:18], data[:, 24:27], data[:, 18:24]), axis=1)
+        elif part =='RightArm':
+            part_data = torch.cat((data[:, 3:9], data[:, 15:18], data[:, 24:27], data[:, 9:15]), axis=1)
+        elif part == 'LeftLeg':
+            part_data = torch.cat((data[:, 3:6], data[:, 24:30], data[:, 36:39], data[:, 39:45]), axis=1)
+        elif part == 'RightLeg':
+            part_data = torch.cat((data[:, 3:6], data[:, 24:30], data[:, 36:39], data[:, 30:36]), axis=1)
+        elif part == 'LeftHand':
+            part_data = torch.cat((data[:, 18:24], data[:, 51:54], data[:, 60:63], data[:, 69:72], data[:, 78:81], data[:, 87:90]), axis=1)
+        elif part == 'RightHand':
+            part_data = torch.cat((data[:, 9:15], data[:, 96:99], data[:, 105:108], data[:, 114:117], data[:, 123:126], data[:, 132:135]), axis=1)
+        elif part == 'LeftFoot':
+            part_data = torch.cat((data[:, 39:45], data[:, 45:48]), axis=1)
+        elif part == 'RightFoot':
+            part_data = torch.cat((data[:, 30:36], data[:, 48:51]), axis=1)
+        elif part == 'LeftThumb':
+            part_data = torch.cat((data[:, 21:24], data[:, 51:60]), axis=1)
+        elif part == 'LeftIndex':
+            part_data = torch.cat((data[:, 21:24], data[:, 60:69]), axis=1)
+        elif part == 'LeftMiddle':
+            part_data = torch.cat((data[:, 21:24], data[:, 69:78]), axis=1)
+        elif part == 'LeftRing':
+            part_data = torch.cat((data[:, 21:24], data[:, 78:87]), axis=1)
+        elif part == 'LeftPinky':
+            part_data = torch.cat((data[:, 21:24], data[:, 87:96]), axis=1)
+        elif part == 'RightThumb':
+            part_data = torch.cat((data[:, 12:15], data[:, 96:105]), axis=1)
+        elif part == 'RightIndex':
+            part_data = torch.cat((data[:, 12:15], data[:, 105:114]), axis=1)
+        elif part == 'RightMiddle':
+            part_data = torch.cat((data[:, 12:15], data[:, 114:123]), axis=1)
+        elif part == 'RightRing':
+            part_data = torch.cat((data[:, 12:15], data[:, 123:132]), axis=1)
+        elif part == 'RightPinky':
+            part_data = torch.cat((data[:, 12:15], data[:, 132:141]), axis=1)
+
+
+        return part_data
+
+    def cat_numpy(self, part, data):
+        if part == 'Torso':
+            part_data = np.concatenate((data[:, 0:9], data[:, 15:18], data[:, 24:30], data[:, 36:39]), axis=1)
+        elif part == 'LeftArm':
+            part_data = np.concatenate((data[:, 3:9], data[:, 15:18], data[:, 24:27], data[:, 18:24]), axis=1)
+        elif part =='RightArm':
+            part_data = np.concatenate((data[:, 3:9], data[:, 15:18], data[:, 24:27], data[:, 9:15]), axis=1)
+        elif part == 'LeftLeg':
+            part_data = np.concatenate((data[:, 3:6], data[:, 24:30], data[:, 36:39], data[:, 39:45]), axis=1)
+        elif part == 'RightLeg':
+            part_data = np.concatenate((data[:, 3:6], data[:, 24:30], data[:, 36:39], data[:, 30:36]), axis=1)
+        elif part == 'LeftHand':
+            part_data = np.concatenate((data[:, 18:24], data[:, 51:54], data[:, 60:63], data[:, 69:72], data[:, 78:81], data[:, 87:90]), axis=1)
+        elif part == 'RightHand':
+            part_data = np.concatenate((data[:, 9:15], data[:, 96:99], data[:, 105:108], data[:, 114:117], data[:, 123:126], data[:, 132:135]), axis=1)
+        elif part == 'LeftFoot':
+            part_data = np.concatenate((data[:, 39:45], data[:, 45:48]), axis=1)
+        elif part == 'RightFoot':
+            part_data = np.concatenate((data[:, 30:36], data[:, 48:51]), axis=1)
+        elif part == 'LeftThumb':
+            part_data = np.concatenate((data[:, 21:24], data[:, 51:60]), axis=1)
+        elif part == 'LeftIndex':
+            part_data = np.concatenate((data[:, 21:24], data[:, 60:69]), axis=1)
+        elif part == 'LeftMiddle':
+            part_data = np.concatenate((data[:, 21:24], data[:, 69:78]), axis=1)
+        elif part == 'LeftRing':
+            part_data = np.concatenate((data[:, 21:24], data[:, 78:87]), axis=1)
+        elif part == 'LeftPinky':
+            part_data = np.concatenate((data[:, 21:24], data[:, 87:96]), axis=1)
+        elif part == 'RightThumb':
+            part_data = np.concatenate((data[:, 12:15], data[:, 96:105]), axis=1)
+        elif part == 'RightIndex':
+            part_data = np.concatenate((data[:, 12:15], data[:, 105:114]), axis=1)
+        elif part == 'RightMiddle':
+            part_data = np.concatenate((data[:, 12:15], data[:, 114:123]), axis=1)
+        elif part == 'RightRing':
+            part_data = np.concatenate((data[:, 12:15], data[:, 123:132]), axis=1)
+        elif part == 'RightPinky':
+            part_data = np.concatenate((data[:, 12:15], data[:, 132:141]), axis=1)
+
+        return part_data
+
+    def combine_numpy(self, part_datas):
+        torso = part_datas['Torso']
+        l_arm = part_datas['LeftArm']
+        l_leg = part_datas['LeftLeg']
+        r_arm = part_datas['RightArm']
+        r_leg = part_datas['RightLeg']
+        l_hand = part_datas['LeftHand']
+        r_hand = part_datas['RightHand']
+        l_foot = part_datas['LeftFoot']
+        r_foot = part_datas['RightFoot']
+        l_thumb = part_datas['LeftThumb']
+        l_index = part_datas['LeftIndex']
+        l_middle = part_datas['LeftMiddle']
+        l_ring = part_datas['LeftRing']
+        l_pinky = part_datas['LeftPinky']
+        r_thumb = part_datas['RightThumb']
+        r_index = part_datas['RightIndex']
+        r_middle = part_datas['RightMiddle']
+        r_ring = part_datas['RightRing']
+        r_pinky = part_datas['RightPinky']
+
+        result = np.concatenate((torso[:, 0:9], r_arm[:, -6:], torso[:, 9:12], l_arm[:, -6:], 
+                                    torso[:, 12:18], r_leg[:, -6:], torso[:, 18:21], l_leg[:, -6:],
+                                    l_foot[:, -3:], r_foot[:, -3:],
+                                    l_hand[:, 6:9], l_thumb[:, -6:], 
+                                    l_hand[:, 9:12], l_index[:, -6:], 
+                                    l_hand[:, 12:15], l_middle[:, -6:], 
+                                    l_hand[:, 15:18], l_ring[:, -6:], 
+                                    l_hand[:, 18:21], l_pinky[:, -6:], 
+                                    r_hand[:, 6:9], r_thumb[:, -6:], 
+                                    r_hand[:, 9:12], r_index[:, -6:], 
+                                    r_hand[:, 12:15], r_middle[:, -6:], 
+                                    r_hand[:, 15:18], r_ring[:, -6:], 
+                                    r_hand[:, 18:21], r_pinky[:, -6:]), 1)
+        
+        return result
+
+"""
+Definition:
+- torso: Head, Neck, RightShoulder, LeftShoulder, Pelvis, RightThigh, LeftThigh (7 joints)
+- left hand: Neck, RightShoulder, LeftShoulder, Pelvis, LeftArm, LeftHand (6 joints)
+- right hand: Neck, RightShoulder, LeftShoulder, Pelvis, RightArm, RightHand (6 joints)
+- left leg: Neck, Pelvis, RightThigh, LeftThigh, LeftKnee, LeftAnkle (6 joints)
+- right leg: Neck, Pelvis, RightThigh, LeftThigh, RightKnee, RightAnkle (6 joints)
+
 - left hand with finger: "LeftArm", "LeftHand",
                         "LeftHandThumb1", "LeftHandThumb2", "LeftHandThumb3",
                         "LeftHandIndex1", "LeftHandIndex2", "LeftHandIndex3",
@@ -101,7 +295,8 @@ class JointDefV5:
 
         result = np.concatenate((torso[:, 0:9], r_arm[:, -6:], torso[:, 9:12], l_arm[:, -6:], 
                                     torso[:, 12:18], r_leg[:, -6:], torso[:, 18:21], l_leg[:, -6:],
-                                    l_hand[:, -45:], r_hand[:, -45:], l_foot[:, -3:], r_foot[:, -3:]), 1)
+                                    l_foot[:, -3:], r_foot[:, -3:],
+                                    l_hand[:, -45:], r_hand[:, -45:]), 1)
         
         return result
 
